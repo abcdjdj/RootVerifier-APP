0dba8e66a2d2507a563d40932e138a7c9786b787
Madhav Kanbur <abcdjdj@gmail.com>
2013-11-07 19:28:30 +0530

    Updated to V1.3

--- src/com/abcdjdj/rootverifier/MainActivity.java
+++ src/com/abcdjdj/rootverifier/MainActivity.java
@@ -19,231 +19,200 @@ along with Root Verifier. If not, see <http://www.gnu.org/licenses/>.*/
 
 package com.abcdjdj.rootverifier;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
-
-
+import android.widget.Toast;
 
 /**
  * @author - Madhav Kanbur (abcdjdj)
- * @version - V1.2
+ * @version - V1.3
  */
 
 public class MainActivity extends Activity 
 {
+    private static String path;
+    private static ProgressDialog dialog;
+    private static File file;
+    //If the file exists, then no need to ask again else ask.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) 
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		setDeviceName();
-		//Calling the function to display the current device model on startup of the app.
-	}
+        onStartUp();
+    }
 
-	
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-	    switch (item.getItemId()) 
-	    {
-	    case R.id.action_settings://If the exit button is pressed then call the finish() function
-	    	this.finish();
-	    	return true;
-	    	
-	    	default:
-	    		return true;
-	    	
-	        
-	    }
-	}
+    public void onStartUp()
+    {
 
-	public void Check(View v) throws IOException, InterruptedException
-	{
-		/*
-		 * This is the main function of the app which is 
-		 * called when the user clicks on the "Check" button.
-		 */
-			
-		TextView a = (TextView) findViewById(R.id.textView1);			
-		if (RootAvailibility())//Checks if su binary is available
-		{
-		
-			try
-			{
-						
-				Process process = Runtime.getRuntime().exec("su");
-				OutputStream stdin = process.getOutputStream();
-				
-				//CREATING A DUMMY FILE in /system called abc.txt
-				stdin.write("mount -o remount rw /system/\n".getBytes());
-				stdin.write("cd system\n".getBytes());
-				stdin.write("echo \"ABC\" > abc.txt\n".getBytes());
-				stdin.write("exit\n".getBytes());
-				stdin.flush();
-				stdin.close();
-				process.waitFor();
-				
-				
-				if(checkFile())//Checks if the file has been successfully created
-				{
-					a.setText("DEVICE IS ROOTED");
-				}
-				else
-				{
-				a.setText("ROOT PERMISSION NOT GRANTED OR SUPERUSER APP MISSING");
+        path=getFilesDir().getPath();
 
-				}
-				
-				process=Runtime.getRuntime().exec("su");
-				stdin=process.getOutputStream();
-				
-				//DELETES THE DUMMY FILE IF PRESENT
-				stdin.write("cd system\n".getBytes());
-				stdin.write("rm abc.txt\n".getBytes());
-				stdin.write("exit\n".getBytes());
-				stdin.flush();
-				stdin.close();
-				process.waitFor();
-				process.destroy();
-								
-			}
-			catch(Exception e)
-			{
-				
-			
-				a.setText("ROOT PERMISSION NOT GRANTED OR SUPERUSER APP MISSING");
-			}
-		}
-		else 
-		{
-			
-			a.setText("NOT ROOTED");
-		}
-		busybox();//To checks if busybox is installed or not
-		
-		
-	}
+        file = new File(path+"/flag.txt");
 
-	public boolean checkFile() 
-	{
-		//Checks if the file has been created successfully
-		File x = new File("/system/abc.txt");
-		boolean flag=x.exists();
-		return flag;
+        CheckRoot.setActivity(this, dialog);
 
-	}
-	
-	
-	public boolean RootAvailibility()throws IOException
-	{
-		
-		try
-		{
-			
-		Process p = Runtime.getRuntime().exec("su");
-		p.destroy();
-		return true;
-		}
-		catch(Exception e)
-		{
-			return false;
-		}
+        setDeviceName();// Calling the function to display the current device model on startup of the app.
+    }
 
-	}
-	
-	
-	
-	public void setDeviceName()
-	{
-		String x;
-		x=android.os.Build.MANUFACTURER + " " + android.os.Build.MODEL;
-		TextView c  = (TextView)findViewById(R.id.devicemodel);
-		//devicemodel is the TextView which displays the device model
-		x=x.toUpperCase();
-		c.setText("DEVICE:- " + x);
-		
-	}
-	
-	
-	public void busybox()throws IOException
-	{
-		/*
-		 * If the busybox process is created successfully,
-		 * then IOException won't be thrown. To get the 
-		 * busybox version, we must read the output of the command
-		 * 
-		 */
-				
-		TextView z = (TextView)findViewById(R.id.busyboxid);
-		String line=null;char n[]=null;
-		
-		try
-		{
-			
-		Process p =Runtime.getRuntime().exec("busybox");
-		InputStream a = p.getInputStream();
-		InputStreamReader read = new InputStreamReader(a);
-		BufferedReader in = new BufferedReader(read);
-		
-		/*
-		 * Labeled while loop so that the while loop
-		 * can be directly broken from the nested for.
-		 * 
-		 */
-		abc :while((line=in.readLine())!=null)
-		{
-			n=line.toCharArray();
-			
-			for(char c:n)
-			{
-				/*
-				 * This nested for loop checks if 
-				 * the read output contains a digit (number),
-				 * because the expected output is -
-				 * "BusyBox V1.xx". Just to make sure that
-				 * the busybox version is read correctly. 
-				 */
-				if(Character.isDigit(c))
-				{
-					break abc;//Once a digit is found, terminate both loops.
-					
-				}
-			}
-			
-		}
-		z.setText("BUSYBOX INSTALLED - " + line);
-		}
-		catch(Exception e)
-		{
-			/*
-			 * IOException has been thrown which indicates that
-			 * that busybox is not installed.
-			 * */
-			
-			z.setText("BUSYBOX NOT INSTALLED OR NOT SYMLINKED");
-		}
-	}
-	
-}//Class Body
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) 
+    {
+        // Handle item selection
+        switch (item.getItemId()) 
+        {
+            case R.id.exit:
+            finish();
+            break;
+
+            case R.id.rate:
+            rateOnPS();            
+            break;
+
+            default:
+            break;
+
+        }
+        return true;
+    }
+
+    public void Check(View v) 
+    {
+
+        dialog = ProgressDialog.show(this, "Verifying root..","Checking. Please wait...", false);
+        dialog.setCanceledOnTouchOutside(false);
+        CheckRoot.setActivity(this, dialog);
+
+        CheckRoot r = new CheckRoot();
+        r.start();
+
+    }
+    public void setDeviceName()
+    {
+        TextView c = (TextView) findViewById(R.id.devicemodel);
+
+        StringBuilder x = new StringBuilder("DEVICE:- ");
+        x.append(android.os.Build.MANUFACTURER).append(" ").append(android.os.Build.MODEL);
+
+        c.setText(x);
+
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(readFlag())
+        {
+            finish();
+        }
+        else
+        {
+            showDialog();
+        }
+    }
+
+    public void showDialog() 
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Rate on Play Store?");
+        builder.setMessage("If you enjoyed using my app, then please support me by rating it on Play Store. Thanks:)");
+        builder.setPositiveButton("Rate now", new OnClickListener(){
+
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) 
+                {
+
+                    rateOnPS();
+                }
+            });
+        builder.setNegativeButton("Later", new OnClickListener(){
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) 
+                {
+                    writeFlag(true);
+                    finish();				
+                }
+
+            });
+        builder.setCancelable(false);
+        builder.show();
+
+    }
+
+    private static void rateOnPS()
+    {
+        Intent intent=null;
+        try
+        {
+            intent= new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.abcdjdj.rootverifier"));
+            CheckRoot.activity.startActivity(intent);
+        }
+        catch(ActivityNotFoundException e)
+        {
+            intent= new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.abcdjdj.rootverifier"));
+            CheckRoot.activity.startActivity(intent);
+        }
+        catch(Exception ex)
+        {
+            Toast.makeText(CheckRoot.activity, "Unknown error occured", Toast.LENGTH_LONG).show();
+
+        }
+        writeFlag(true);
+    }
+
+    private static void writeFlag(boolean create)
+    {
+        try
+        {
+
+            if(create)
+            {
+                file.createNewFile();
+            }
+            else
+            {
+                file.delete();
+            }
+        }
+        catch(Exception e){}
+    }
+
+    private static boolean readFlag()
+    {
+        boolean ans;
+        try
+        {
+            ans= file.exists();
+        }
+        catch(Exception e)
+        {
+            ans=false;//If any error occurs, anyway prompt the user to rate
+        }
+
+        return ans;
+    }
+
+}
